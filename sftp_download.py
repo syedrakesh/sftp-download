29d7ef2918d01a715e05ceef8a6dcb29cb7d6004
syedrakeshuddin <rakeshuddin@nsolutionsys.com>
2023-03-20 08:05:22 +0600

    Time added Success message for all files download added

--- sftp_download.py
+++ sftp_download.py
@@ -1,57 +1,77 @@
 import fnmatch
 import pysftp
+import datetime
 
 # Set up the SFTP connection parameters
-host = 'localhost'
-username = 'root'
+host = '127.0.0.1'
+username = 'rakesh'
 password = 'password'
 
 # Remote Directory Path
-remote_directory = '/usr/sap/tmp/DAM'
+remote_directory = '/home/rakesh/Desktop/Rakesh/remote'
 # Local Directory Path
-local_directory = '/home/rakesh/Desktop/Rakesh'
+local_directory = '/home/rakesh/Desktop/Rakesh/local'
 # File Pattern
-pattern = 'ASSET_DUMP*'
+pattern = 'dam*'
 # File Type
-file_type = '.txt'
+# file_type = '.txt'
+file_type = ''
+
+# Current Time and Date
+now = datetime.datetime.now()
 
 try:
-    cnopts = pysftp.CnOpts()
-    cnopts.hostkeys = None
+    print('Trying To Connect with', host, ' at ', now.strftime("%Y-%m-%d %H:%M:%S"))
+
+    # cnopts = pysftp.CnOpts()
+    # cnopts.hostkeys = None
+
     # Establish a connection using the above parameters
-    # with pysftp.Connection(host=host, username=username, password=password) as sftp:
-    with pysftp.Connection(host=host, username=username, password=password, cnopts=cnopts) as sftp:
+    # with pysftp.Connection(host=host, username=username, password=password, cnopts=cnopts) as sftp:
+    with pysftp.Connection(host=host, username=username, password=password) as sftp:
+        print('Connected Successfully at ', now.strftime("%d-%m-%Y %H:%M:%S"), '\n')
         try:
             # change the directory
             sftp.chdir(remote_directory)
+            print('Entered Target Directory Successfully.')
 
             # get a list of all files in the remote directory
             file_list = sftp.listdir()
+            # print(file_list)
 
-            # create a list of files that match the pattern
+            # Create a list of files that match the pattern
             pattern = pattern + file_type
             matched_files = fnmatch.filter(file_list, pattern)
+            print('Number of Matched Files -> ', len(matched_files), '\n')
 
             # if the file list is empty, print a message and exit
             if not matched_files:
                 print("No matched files found in the remote directory.")
                 exit()
 
-            # download each matched file to the local directory
+            # Download each matched file to the local directory
+            counter = 1
             for file_name in matched_files:
                 try:
-                    # download the file
-                    print(file_name, '-> Start Downloading.')
+                    # Download the file
+                    print(++counter, '. Matched File Name -> ', file_name)
+                    print(file_name, '-> Start Downloading at ', now.strftime("%d-%m-%Y %H:%M:%S"), '\n')
                     sftp.get(file_name, f'{local_directory}/{file_name}')
-                    print(file_name, '-> Successfully Downloaded.')
+                    print(file_name, '-> Successfully Downloaded at ', now.strftime("%d-%m-%Y %H:%M:%S"), '\n')
 
                 except Exception as e:
-                    # print an error message
+                    # Print an error message
                     print(f"Error downloading {file_name}: {e}")
+
+                print('All Matched Files Downloaded Successfully! at ', now.strftime("%d-%m-%Y %H:%M:%S"), '\n')
+
         except Exception as e:
-            # handle any other exceptions that may occur
+            # Handle any other exceptions that may occur
             print(f"An Directory error occurred: {e}")
 
+    print('Connection Closed.')
+    sftp.close()
+
 except Exception as e:
-    # handle the exception here
+    # Handle the exception here
     print(f"An SFTP error occurred: {e}")
